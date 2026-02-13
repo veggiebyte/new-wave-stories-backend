7033b08a3974591452831c1cc2a502b2e35233e5
Cindy Stadterman <veggiebyte@yahoo.com>
2026-02-13 14:51:15 -0800

    Added Schema file

--- db_helpers.py
+++ db_helpers.py
@@ -3,10 +3,16 @@ import psycopg2
 
 
 def get_db_connection():
-    connection = psycopg2.connect(
-        host='localhost',
-        database=os.getenv('POSTGRES_DATABASE'),
-        user=os.getenv('POSTGRES_USERNAME'),
-        password=os.getenv('POSTGRES_PASSWORD')
-    )
+    if 'ON_HEROKU' in os.environ:
+        connection = psycopg2.connect(
+            os.getenv('DATABASE_URL'), 
+            sslmode='require'
+        )
+    else:
+        connection = psycopg2.connect(
+            host='localhost',
+            database=os.getenv('POSTGRES_DATABASE'),
+            user=os.getenv('POSTGRES_USERNAME'),
+            password=os.getenv('POSTGRES_PASSWORD')
+        )
     return connection
